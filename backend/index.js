04d477d579ec26a2d2b2a18a094b4d67f44ee382
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2025-05-31 13:11:42 -0400

    songs.controller added with getSongs, putSong,DeleteSong and postSong;

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,7 @@ import express from "express";
 import cors from "cors";
 import { readFile, writeFile } from "fs/promises";
 import { nanoid } from "nanoid";
-import dotenv from "dotenv";
-
-dotenv.config();
+import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,8 +12,12 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get("/api/repertorio", async (req, res) => {
+app.get("/canciones", getSongs);
 
-})
+app.post("/canciones", postSong);
 
+app.put("/canciones/:id", putSong);
 
+app.delete("/canciones/:id", deleteSong);
+
+app.listen(PORT, console.log(`Servidor corriendo en http://localhost:${PORT}`))
---
+++ backend/src/controllers/songs.controller.js
@@ -0,0 +1,69 @@
+import path from "path";
+import fs from "fs";
+import { json } from "stream/consumers";
+
+const repertorioPath = path.resolve("repertorio.json");
+
+const getSongs = async () => {
+    const songs = JSON.parse(fs.readFileSync(repertorioPath, "utf-8"));
+    res.json(songs);
+}
+
+const postSong = async (req, res) => {
+    try{
+        const { titulo, artista } = req.body;
+        const songs = JSON.parse(fs.readFileSync(repertorioPath, "utf-8"));
+        const nuevaCancion = { id: nanoid(), titulo, artista };
+        songs.push(nuevaCancion);
+
+        fs.writeFileSync(repertorioPath, JSON.stringify(songs));
+
+        res.send("Canción agregada correctamente");
+    } catch (error) {
+        res.json({ error: "Error al agregar la canción" });
+        return;
+    }
+
+    await writeFile(repertorioPath, JSON.stringify(data, null, 2));
+    res.status(201).json(nuevaCancion);
+}
+
+
+const putSong = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { titulo, artista } = req.body;
+
+        let songs = JSON.parse(fs.readFileSync(repertorioPath, "utf-8"));
+
+        songs = songs.map((c) =>
+            c.id === id ? { ...c, titulo, artista } : c
+        );
+
+        fs.writeFileSync(repertorioPath, JSON.stringify(songs));
+
+        res.json({ mensaje: "Canción actualizada" });
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar la canción" });
+        return;
+    }
+
+}
+
+const deleteSong = async (req, res) => {
+    try{
+        const { id } = req.params;
+        let songs = JSON.parse(fs.readFileSync(repertorioPath, "utf-8"));
+
+        const nuevasCanciones = songs.filter((c) => c.id !== id);
+        fs.writeFileSync(repertorioPath, JSON.stringify(nuevasCanciones));
+
+        res.json({ mensaje: "Canción eliminada" });
+    } catch (error) {
+        res.status(500).json({ error: "Error al eliminar la canción" });
+        return;
+    }
+
+}
+
+export {deleteSong, getSongs, postSong, putSong}
